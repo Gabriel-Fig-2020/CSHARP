435680b3af7d8fee868c46e4dd415f6a0effead8
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2020-12-29 23:56:42 -0300

    estudos 29/12

---
+++ DigitalInovationOne/DigitalInovationOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVe=5;
+            for(int i=0; i<numeroDeVe;i++){
+                Console.WriteLine($"Bem vindos {i}");
+            }
+        }
+    }
+}
